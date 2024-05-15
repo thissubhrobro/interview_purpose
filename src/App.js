eaf9fb1acb06e7238cf90bb65555664c4fd90fcf
Subhronil Chatterjee <subhronil.chatterjee@b4b.ai>
2024-05-15 14:04:06 +0530

    api fetched and added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const url = "http://universities.hipolabs.com/search?country=India";
+  const [details, setDetails] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = response.json();
+    setDetails(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {details.map((value) => (
+        <table>
+          <tr>
+            <th>Country</th>
+            <th>Web Pages</th>
+            <th>Alpha Two Code</th>
+            <th>State Province</th>
+            <th>Domains</th>
+            <th>Name</th>
+          </tr>
+          <tr>
+            <td>{value.country}</td>
+            <td>{value.web_pages[0]}</td>
+            <td>{value.alpha_two_code}</td>
+            <td>{value["state-province"]}</td>
+            <td>{value.domains[0]}</td>
+            <td>{value.name}</td>
+          </tr>
+        </table>
+      ))}
     </div>
   );
 }
